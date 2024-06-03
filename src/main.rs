dcec470029bd81c928d06ee1243a0f364760d0f4
Juno <pawelekstanley@gmail.com>
2024-06-03 20:39:58 +0200

    Save to file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::{env, io};
-use std::io::BufRead;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, Write};
 
 fn main() {
+    let mut file = File::create("dzida.txt").unwrap();
+
     println!("Podaj przedmiot: (z dużej)");
     let binding = io::stdin().lock().lines().next().unwrap().unwrap();
     let item = binding.trim();
@@ -15,15 +18,15 @@ fn main() {
     let count = io::stdin().lock().lines().next().unwrap().unwrap()
         .trim().parse::<usize>().expect("Niepoprawna wartość");
 
-    dzida(item, dop, count);
+    dzida(item, dop, count, &mut file);
 }
 
-fn dzida(mianownik: &str, dopelniacz: &str, count: usize) {
+fn dzida(mianownik: &str, dopelniacz: &str, count: usize, file: &mut File) {
     if count <= 0 {
         return;
     }
-    println!("{} składa się z przedniej części {}, środkowej części {} i tylnej części {}.", mianownik, dopelniacz, dopelniacz, dopelniacz);
-    dzida(&("Przednia część ".to_owned() + dopelniacz), &("przedniej części ".to_owned() + dopelniacz), count - 1);
-    dzida(&("Środkowa część ".to_owned() + dopelniacz), &("środkowej części ".to_owned() + dopelniacz), count - 1);
-    dzida(&("Tylna część ".to_owned() + dopelniacz), &("tylnej części ".to_owned() + dopelniacz), count - 1);
+    file.write_all(format!("{} składa się z przedniej części {}, środkowej części {} i tylnej części {}.\n", mianownik, dopelniacz, dopelniacz, dopelniacz).as_bytes()).unwrap();
+    dzida(&("Przednia część ".to_owned() + dopelniacz), &("przedniej części ".to_owned() + dopelniacz), count - 1, file);
+    dzida(&("Środkowa część ".to_owned() + dopelniacz), &("środkowej części ".to_owned() + dopelniacz), count - 1, file);
+    dzida(&("Tylna część ".to_owned() + dopelniacz), &("tylnej części ".to_owned() + dopelniacz), count - 1, file);
 }
